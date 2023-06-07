f1ac64acd1c60644c2aed7dba1e330787e715936
EstefaniSoto <eri022004@hotmail.com>
2023-06-07 19:51:53 -0400

    json en js

--- alertas.js
+++ alertas.js
@@ -12,7 +12,22 @@ function validarFormulario(e){
     alert(`${nombre}, tus tickets para la pelicula *${pelicula}* han sido reservados con exito!`)
     console.log(nombre)
 }
+const database = `[
+    {
+        "nombre": "paola",
+        "numero": "8296058217",
+        "correo": "dp673162@gmail.com",
+        "pelicula": "SUPER MARIO BROS (2D)",
+        "tanda": "5:30PM-7:00PM",
+        "fecha": "16/06/2023"
+    }]`
 
+    console.log(typeof database)
+   
+    const jsonData = JSON.parse(database);
+    console.log(typeof jsonData);
+
+    console.log(database);
 // const fs = require('fs');
 
 // // Leer el contenido del archivo JSON
