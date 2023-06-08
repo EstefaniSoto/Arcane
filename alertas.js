49ea62e38e112e3c97893506af76216b77411b4b
paola1611 <dp673162@gmail.com>
2023-06-07 20:20:52 -0400

    actualizando archivos JSON

--- alertas.js
+++ alertas.js
@@ -1,18 +1,24 @@
 const formulario = document.querySelector("#formulario");
-formulario.addEventListener("submit", validarFormulario)
+formulario.addEventListener("submit", validarFormulario);
 
-function validarFormulario(e){
-    e.preventDefault();
-    nombre = document.querySelector("#nombre").value
-    pelicula = document.querySelector("#pelicula").value
-    fecha = document.querySelector("#fecha").value
-    localStorage.setItem("nombreUsuario", nombre);
-    localStorage.setItem("pelicula", pelicula);
-    localStorage.setItem("fecha", fecha);
-    alert(`${nombre}, tus tickets para la pelicula *${pelicula}* han sido reservados con exito!`)
-    console.log(nombre)
-}
-const database = `[
+function validarFormulario(e) {
+  e.preventDefault();
+  const nombre = document.querySelector("#nombre").value;
+  const pelicula = document.querySelector("#pelicula").value;
+  const fecha = document.querySelector("#fecha").value;
+  localStorage.setItem("nombreUsuario", nombre);
+  localStorage.setItem("pelicula", pelicula);
+  localStorage.setItem("fecha", fecha);
+  alert(`${nombre}, tus tickets para la pelicula *${pelicula}* han sido reservados con exito!`);
+  
+  // Obtén los datos del Local Storage
+  const localStorageData = {
+    nombre: localStorage.getItem("nombreUsuario"),
+    pelicula: localStorage.getItem("pelicula"),
+    fecha: localStorage.getItem("fecha"),
+  };
+  
+  const database = `[
     {
         "nombre": "paola",
         "numero": "8296058217",
@@ -21,20 +27,16 @@ const database = `[
         "tanda": "5:30PM-7:00PM",
         "fecha": "16/06/2023"
     }]`
+  // Actualiza los datos del array jsonData con los datos del Local Storage
+  const jsonData = JSON.parse(database);
+  jsonData[0] = localStorageData;
 
-    console.log(typeof database)
-   
-    const jsonData = JSON.parse(database);
-    console.log(typeof jsonData);
+  console.log(jsonData); // Array JSON actualizado
 
-    console.log(database);
-// const fs = require('fs');
+  // Convierte el array JSON de nuevo a formato de cadena de texto
+  const updatedData = JSON.stringify(jsonData);
+  console.log(updatedData); // Cadena de texto JSON actualizada
 
-// // Leer el contenido del archivo JSON
-// const contenidoJSON = fs.readFileSync('./database.json', 'utf8');
-
-// // Analizar el contenido JSON en un objeto JavaScript
-// const objetoJSON = JSON.parse(contenidoJSON);
-
-// // Utilizar el objeto JS
-// console.log(objetoJSON);
+  // Guarda los datos actualizados en el Local Storage si es necesario
+  // localStorage.setItem('nombreDelArray', updatedData);
+}
