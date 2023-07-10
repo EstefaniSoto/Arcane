d94da04c3ce3d9ee98830e82ed0c2eea085fe425
20211859 <20211859@itla.edu.do>
2023-07-10 16:43:11 -0400

    Modificando snacks stage

--- snacks/main.js
+++ snacks/main.js
@@ -1,7 +1,21 @@
 const button = document.querySelector(".order-submit-btn");
 
-const prevent = (e) => {
+const updateFormAction = (e) => {
+    const form = document.getElementById("form-action");
+    const emailInput = document.getElementById("emailInput");
+    const nombreInput = document.querySelector('input[name="nombre"]');
+    const comidaSelect = document.querySelector('select[name="comida"]');
+    const cantidadSelect = document.querySelector('select[name="cantidad"]');
+    const tarjetaInput = document.querySelector('input[name="tarjeta"]');
     e.preventDefault();
-}
 
-button.addEventListener("click", prevent);
+    if (nombreInput.value === '' || comidaSelect.value === 'f0' || cantidadSelect.value === 'o1' || tarjetaInput.value === '' || emailInput.value === '') {
+        alert("Por favor, complete todos los campos");
+        return;
+    }
+
+    alert("Compra éxitosa");
+  }
+
+button.addEventListener("click", updateFormAction);
+
